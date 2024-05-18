9e2851f60fbe0e68dbb8fe311250d000daa4af5f
MarcondesOliveira <marcondescurso@outlook.com>
2024-05-18 20:38:24 -0300

    interface

--- main.go
+++ main.go
@@ -5,10 +5,24 @@ import (
 	"fmt"
 )
 
+func PagarBoleto(conta verificarConta, valorDoBoleto float64) {
+	conta.Sacar(valorDoBoleto)
+}
+
+type verificarConta interface {
+	Sacar(valor float64) string
+}
+
 func main() {
 	contaDoDenis := contas.ContaPoupanca{}
 	contaDoDenis.Depositar(100)
-	contaDoDenis.Sacar(5000)
+	PagarBoleto(&contaDoDenis, 60)
 
 	fmt.Println(contaDoDenis.ObterSaldo())
+
+	contaDaLuiza := contas.ContaCorrente{}
+	contaDaLuiza.Depositar(500)
+	PagarBoleto(&contaDaLuiza, 1000)
+
+	fmt.Println(contaDaLuiza.ObterSaldo())
 }
