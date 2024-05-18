4c24d47f3814f1867bb6dd2cf4c315482e1eee87
MarcondesOliveira <marcondescurso@outlook.com>
2024-05-18 13:12:53 -0300

    comparando tipos

--- main.go
+++ main.go
@@ -10,28 +10,40 @@ type ContaCorrente struct {
 }
 
 func main() {
-	contaDoTroll := ContaCorrente{
-		titular:       "Troll",
-		numeroAgencia: 589,
-		numeroConta:   123456,
-		saldo:         125.05,
-	}
+	// contaDoTroll := ContaCorrente{
+	// 	titular:       "Troll",
+	// 	numeroAgencia: 589,
+	// 	numeroConta:   123456,
+	// 	saldo:         125.05,
+	// }
 
-	contaDoZezinho := ContaCorrente{
-		"Zezinho",
-		222,
-		111222,
-		200,
-	}
+	// contaDoZezinho := ContaCorrente{
+	// 	"Zezinho",
+	// 	222,
+	// 	111222,
+	// 	200,
+	// }
 
-	fmt.Println(contaDoTroll)
-	fmt.Println(contaDoZezinho)
+	
+
+	// fmt.Println(contaDoTroll)
+	// fmt.Println(contaDoZezinho)
 
 	var contaDoColosso *ContaCorrente
 	contaDoColosso = new(ContaCorrente)
 	contaDoColosso.titular = "Colosso"
 	contaDoColosso.saldo = 500
 
-	fmt.Println(*contaDoColosso)
+	var contaDoColosso2 *ContaCorrente
+	contaDoColosso2 = new(ContaCorrente)
+	contaDoColosso2.titular = "Colosso"
+	contaDoColosso2.saldo = 500
+
+	// fmt.Println(*contaDoColosso)
+
+	fmt.Println(&contaDoColosso)
+	fmt.Println(&contaDoColosso2)
+
+	fmt.Println(*contaDoColosso == *contaDoColosso2)
 
 }
