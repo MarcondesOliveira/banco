c5e7961cb4b85b9d226370b97e04034e60ef90f0
MarcondesOliveira <marcondescurso@outlook.com>
2024-05-18 13:44:57 -0300

    Adicionando função Sacar

--- main.go
+++ main.go
@@ -9,41 +9,26 @@ type ContaCorrente struct {
 	saldo         float64
 }
 
+func (c *ContaCorrente) sacar(valorDoSaque float64) string {
+	podeSacar := valorDoSaque > 0 && valorDoSaque <= c.saldo
+
+	if podeSacar {
+		c.saldo -= valorDoSaque
+		return "Saque realizado com sucesso"
+	} else {
+		return "Salvo insuficiente"
+	}
+}
+
 func main() {
-	// contaDoTroll := ContaCorrente{
-	// 	titular:       "Troll",
-	// 	numeroAgencia: 589,
-	// 	numeroConta:   123456,
-	// 	saldo:         125.05,
-	// }
+	contadaSilvia := ContaCorrente{}
+	contadaSilvia.titular = "Silvia"
+	contadaSilvia.saldo = 500
 
-	// contaDoZezinho := ContaCorrente{
-	// 	"Zezinho",
-	// 	222,
-	// 	111222,
-	// 	200,
-	// }
+	fmt.Println(contadaSilvia.saldo)
 
-	
+	fmt.Println(contadaSilvia.sacar(400))
 
-	// fmt.Println(contaDoTroll)
-	// fmt.Println(contaDoZezinho)
-
-	var contaDoColosso *ContaCorrente
-	contaDoColosso = new(ContaCorrente)
-	contaDoColosso.titular = "Colosso"
-	contaDoColosso.saldo = 500
-
-	var contaDoColosso2 *ContaCorrente
-	contaDoColosso2 = new(ContaCorrente)
-	contaDoColosso2.titular = "Colosso"
-	contaDoColosso2.saldo = 500
-
-	// fmt.Println(*contaDoColosso)
-
-	fmt.Println(&contaDoColosso)
-	fmt.Println(&contaDoColosso2)
-
-	fmt.Println(*contaDoColosso == *contaDoColosso2)
+	fmt.Println(contadaSilvia.saldo)
 
 }
